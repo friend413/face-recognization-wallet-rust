4ffefe634c8593eae7eba5a4dba4f4ef63ad7614
friend413 <shinjirohara2@gmail.com>
2024-05-28 03:29:17 +0000

    routes

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,4 +1,10 @@
 use actix_web::{web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct WalletInfo {
+    feature_info: String
+}
 
 pub async fn index() -> impl Responder {
     HttpResponse::Ok().body("Welcome to the face-recognization rust server!")
@@ -6,4 +12,20 @@ pub async fn index() -> impl Responder {
 
 pub async fn status() -> impl Responder {
     HttpResponse::Ok().body("Status: Running")
+}
+
+pub async fn get_wallet_post(info: web::Json<WalletInfo>) -> impl Responder {
+    let response_message = format!(
+        "Got info for: feature - {}",
+        info.feature_info
+    );
+    HttpResponse::Ok().body(response_message)
+}
+
+pub async fn create_wallet_post(info: web::Json<WalletInfo>) -> impl Responder {
+    let response_message = format!(
+        "Created wallet for: feature_info - {}",
+        info.feature_info
+    );
+    HttpResponse::Ok().body(response_message)
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,12 @@
 use crate::controllers::*;
 use actix_web::web;
+
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("")
             .route("/", web::get().to(index)) // GET request to "/"
             .route("/status", web::get().to(status)) // GET request to "/status"
+            .route("/get_wallet", web::post().to(get_wallet_post))
+            .route("/create_wallet", web::post().to(create_wallet_post)) 
     );
 }
