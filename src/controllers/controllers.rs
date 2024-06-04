6d837c25002265a876f5a489662c755f0feab757
friend413 <shinjirohara2@gmail.com>
2024-06-04 14:33:10 +0000

    added jwt token for creating wallet

--- src/controllers/controllers.rs
+++ src/controllers/controllers.rs
@@ -3,11 +3,22 @@ use cess_rust_sdk::core::utils::account::get_pair_address_as_ss58_address;
 use serde::{Deserialize, Serialize};
 use sp_keyring::sr25519::sr25519::Pair;
 use subxt_signer::bip39::Mnemonic;
-use crate::controllers::accounts::{generate_mnemonic, get_pair};
+use crate::{
+    controllers::accounts::{generate_mnemonic, get_pair},
+    jwt::{generate_token, is_valid}
+};
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct WalletInfo {
-    feature_info: String
+    uid: u64
+}
+
+#[derive(Serialize, Debug)]
+pub struct WalletResponse {
+    result: String,
+    msg: String,
+    wallet_address: String,
+    token: String
 }
 
 pub async fn index() -> impl Responder {
@@ -21,50 +32,76 @@ pub async fn status() -> impl Responder {
 pub async fn get_wallet_post(info: web::Json<WalletInfo>) -> impl Responder {
     let response_message = format!(
         "address: {}",
-        info.feature_info
+        info.uid
     );
     HttpResponse::Ok().body(response_message)
 }
 
 pub async fn create_wallet_post(info: web::Json<WalletInfo>) -> impl Responder {
     let mnemonic: Option<String>;
-    println!("hello there1");
+
     match generate_mnemonic() {
         Ok(t) => mnemonic = Some(t),
         Err(_) => {
-            return HttpResponse::Ok().content_type("application/json").json("internal error on `generate_mnemonic`");
+            let response_message = WalletResponse{
+                result: "Error".to_string(),
+                msg: "Internal error on `generate_mnemonic`".to_string(),
+                wallet_address: "".to_string(),
+                token: "".to_string()
+            };
+            return HttpResponse::Ok().content_type("application/json").json(response_message);
         }
     };
 
-    println!("hello there2");
     let pair: Pair;
     match get_pair(&mnemonic.clone().unwrap(), None) {
         Ok(t) => pair = t,
         Err(_) => {
-            return HttpResponse::Ok().content_type("application/json").json("internal error on `get_pair`");
+            let response_message = WalletResponse{
+                result: "Error".to_string(),
+                msg: "Internal error on `get_pair`".to_string(),
+                wallet_address: "".to_string(),
+                token: "".to_string()
+            };
+            return HttpResponse::Ok().content_type("application/json").json(response_message);
         }
     };
 
-    println!("hello there3");
     let address_to_fund: String;
     match get_pair_address_as_ss58_address(pair) {
         Ok(t) => address_to_fund = t,
         Err(_) => {
-            return HttpResponse::Ok().content_type("application/json").json("internal error on `get_pair_address_as_ss58_address`");
+            let response_message = WalletResponse{
+                result: "Error".to_string(),
+                msg: "Internal error on `get_pair_address_as_ss58_address`".to_string(),
+                wallet_address: "".to_string(),
+                token: "".to_string()
+            };
+            return HttpResponse::Ok().content_type("application/json").json(response_message);
         }
     }
-    // let address = Some(address_to_fund.clone());
-    println!("hello there4");
 
-    #[derive(Serialize, Debug)]
-    pub struct WalletResponse {
-        wallet_address: String,
+    match generate_token(address_to_fund.clone(), info.uid.clone()) {
+        Ok(token) => {
+            let response_message = WalletResponse {
+                result: "Success".to_string(),
+                msg: "Created wallet successfully".to_string(),
+                wallet_address: address_to_fund,
+                token
+            };
+        
+            println!("test response_message: {:?}", response_message);
+            HttpResponse::Ok().json(response_message)
+        },
+        Err(_) => {
+            let response_message = WalletResponse{
+                result: "Error".to_string(),
+                msg: "Internal error on `generate_token`".to_string(),
+                wallet_address: "".to_string(),
+                token: "".to_string()
+            };
+        
+            HttpResponse::Ok().json(response_message)
+        }
     }
-
-    let response_message = WalletResponse {
-        wallet_address: address_to_fund
-    };
-
-    println!("{:?}", response_message);
-    HttpResponse::Ok().json(response_message)
 }
