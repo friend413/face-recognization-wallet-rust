049950039458724d7126f14ad32aeea7f13c47c3
friend413 <shinjirohara2@gmail.com>
2024-07-18 18:28:04 +0000

    postgres instead of cess db

--- src/databases/mod.rs
+++ src/databases/mod.rs
@@ -2,12 +2,14 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
-use models::{NewAccount, Account};
-use crate::schema::account;
 
+// Assuming `models` and `schema` are modules defined at the same level as this file.
 pub mod models;
-// pub mod schema;
 
+use crate::databases::models::{NewAccount, Account};  // Correcting the path if necessary
+use crate::schema::account;  // This might need to be corrected based on your project structure
+
+// Function to establish a connection to the PostgreSQL database.
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
@@ -16,13 +18,25 @@ pub fn establish_connection() -> PgConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
-pub fn create_account(conn: &mut PgConnection, uid: i64, mnemonic: Option<&str>, address: Option<&str>, token: Option<&str>) -> Account {
+// Function to create a new account in the database.
+pub fn create_account(
+    conn: &mut PgConnection, 
+    uid: i64, 
+    mnemonic: Option<&str>, 
+    address: Option<&str>, 
+    token: Option<&str>, 
+    feature: Option<&[u8]>) -> Account {
 
-    let new_post = NewAccount { uid, mnemonic, address, token };
+    let new_account = NewAccount { 
+        uid, 
+        mnemonic, 
+        address, 
+        token,
+        feature  // Passing the binary data for the feature
+    };
 
     diesel::insert_into(account::table)
-        .values(&new_post)
-        .returning(Account::as_returning())
+        .values(&new_account)
         .get_result(conn)
-        .expect("Error saving new post")
+        .expect("Error saving new account")
 }
